362d7c9c3ae0dbd5fde64dfc4865ce89d725303b
tehmeena <35588433+tehmeena@users.noreply.github.com>
2024-07-11 19:16:24 -0400

    Update script.js
    give user option to select their role once they have submitted their sign up form

--- script.js
+++ script.js
@@ -31,4 +31,21 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('signup-form').style.display = 'none';
         document.getElementById('login-signup-buttons').style.display = 'none';
     });
+
+    // Sign-up form submission - show role selection after submiting sign up info 
+    document.getElementById('signup-form').addEventListener('submit', function(event) {
+        event.preventDefault(); // prevent form submission for now bc this is prototype
+        document.getElementById('signup-form').style.display = 'none';
+        document.getElementById('role-selection').style.display = 'block';
+    });
+
+    //What happens when user engages with the buttons to select their role 
+    document.querySelectorAll('.role-button').forEach(function(button) {
+        button.addEventListener('click', function() {
+            const selectedRole = this.getAttribute('data-role');
+            alert(`You selected: ${selectedRole}`); //just alerting what they selected for now until I add more logic
+        });
+    });
+
+
 });
