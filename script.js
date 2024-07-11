1ab450c8b24e8e410f237a93b7768d98d3dbabb7
tehmeena <35588433+tehmeena@users.noreply.github.com>
2024-07-11 17:43:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('login-button').addEventListener('click', function() {
+        document.getElementById('login-form').style.display = 'block';
+    });
+});
