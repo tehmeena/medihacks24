b864bbb3fd21e3ef21b6669974c3d62f6fa34531
tehmeena <35588433+tehmeena@users.noreply.github.com>
2024-07-11 18:12:15 -0400

    Update script.js
    display the login and sign up forms when the login and sign up buttons are clicked

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+
+// Show sign-up form when sign-up button is clicked
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('login-button').addEventListener('click', function() {
+    document.querySelector('.signup-button').addEventListener('click', function() {
+        document.getElementById('signup-form').style.display = 'block';
+        document.getElementById('login-form').style.display = 'none'; // Hide login form 
+    });
+
+    // Show login form when login button is clicked
+    document.querySelector('.login-button').addEventListener('click', function() {
         document.getElementById('login-form').style.display = 'block';
+        document.getElementById('signup-form').style.display = 'none'; // Hide sign-up form 
     });
 });
