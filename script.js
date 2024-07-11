b6b25d8c3df0f30be1e006e622a3d0eea7910e97
tehmeena <35588433+tehmeena@users.noreply.github.com>
2024-07-11 18:31:21 -0400

    Update script.js
    hide login and sign up buttons once user clicks on them and just display the login/sign up forms

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.signup-button').addEventListener('click', function() {
         document.getElementById('signup-form').style.display = 'block';
         document.getElementById('login-form').style.display = 'none'; // Hide login form 
+        document.getElementById('login-signup-buttons').style.display = 'none';
+
     });
 
     // Show login form when login button is clicked
     document.querySelector('.login-button').addEventListener('click', function() {
         document.getElementById('login-form').style.display = 'block';
         document.getElementById('signup-form').style.display = 'none'; // Hide sign-up form 
+        document.getElementById('login-signup-buttons').style.display = 'none';
+
     });
 });
