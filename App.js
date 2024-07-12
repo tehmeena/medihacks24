f301ba3af0aabb236c805a0c09dfcf8f16126db0
tehmeena <35588433+tehmeena@users.noreply.github.com>
2024-07-12 17:21:36 -0400

    Add files via upload
    Trying to convert my html, css js to react

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <section id="section1">
+        <header>Welcome</header>
+        <p>Hello world for now!</p>
+        <button>Login</button>
+        <button>Sign Up</button>
+      </section>
+    </div>
+  );
+};
+
+export default App;
